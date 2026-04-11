3aa11365d54e1cdb70184176423709c208a352fa
Diego Forganes <diforg@gmail.com>
2026-04-11 11:15:56 -0300

    chore: set version to 0.1.0-beta on release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.0-dev"
+        versionName = "0.1.0-beta"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
